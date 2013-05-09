006d0173af66f39d00b6fd86f22d27fdc272999f
ftgod <student@ent-v321-002.local>
2013-05-08 20:22:35 -0400

    a bunch of files in Python

---
+++ hello.py
@@ -0,0 +1,21 @@
+print("Hello World!")
+
+"""this is a comment I believe"""
+
+a = 100
+b = 1.0
+c = 1 + b
+d = int("1")
+print(a)
+print(b)
+print(c)
+
+s = raw_input('>')
+print(type(s))
+
+# print('what is your name?' + Frank Mason)
+
+t = 'really loud'
+loud = raw_input('how loud?')
+
+print('this is ' + s + '!' + int(loud))
---
+++ mad_libs_lyrics.py
@@ -0,0 +1,40 @@
+"""
+mad_libs_lyrics.py
+====
+* using your lyrics from lyrics.py, replace 4 words with variables
+* set your variables equal to strings at the top of the file
+* name the variables after the part of speech of the word they are replacing,
+  followed by a number:
+
+  adjective1 = 'tiny'
+  adjective2 = 'magical'
+  noun1 = 'ukulele'
+
+* print the new lyrics with variables to the console
+"""
+
+
+a = 'would'
+b = 'you'
+c = 'believe'
+d = 'in'
+
+print('would' + a)
+print('you' + b)
+print('believe' + c)
+print('in' + d)
+
+
+# a = raw_input('would')
+# print(type(a))
+
+# b = raw_input('you')
+# print(type(b))
+
+# c = raw_input('believe')
+# print(type(c))
+
+# d = raw_input('in')
+# print(type(d))"""
+
+# Would you believe in
